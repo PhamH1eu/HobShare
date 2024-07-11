a17367f00752e06d816b34ff5dafaf1ea974a9b4
Phạm Minh Hiếu 21020319 <21020319@vnu.edu.vn>
2024-07-11 23:07:22 +0700

    accordion list

--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -2,6 +2,10 @@ import { auth } from "../../lib/firebase";
 import { useChatStore } from "../../store/chatStore";
 import { useUserStore } from "../../store/userStore";
 import BlockUser from "src/services/BlockUser";
+import Accordion from "@mui/material/Accordion";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 import "./detail.css";
 
 export const Detail = () => {
@@ -33,40 +37,56 @@ export const Detail = () => {
       </div>
       <div className="info">
         <div className="option">
-          <div className="title">
-            <span>Chat Settings</span>
-            <img src="./arrowUp.png" alt="" />
-          </div>
-        </div>
-        <div className="option">
-          <div className="title">
-            <span>Privacy & help</span>
-            <img src="./arrowUp.png" alt="" />
-          </div>
-        </div>
-        <div className="option">
-          <div className="title">
-            <span>Shared photos</span>
-            <img src="./arrowDown.png" alt="" />
-          </div>
-          <div className="photos">
-            <div className="photoItem">
-              <div className="photoDetail">
-                <img
-                  src="https://images.pexels.com/photos/7381200/pexels-photo-7381200.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
-                  alt=""
-                />
-                <span>photo_2024_2.png</span>
+          <Accordion>
+            <AccordionSummary
+              style={{ padding: "0", color: "white" }}
+              expandIcon={<KeyboardArrowDownIcon />}
+            >
+              <div className="title">
+                <span>Chat Settings</span>
               </div>
-              <img src="./download.png" alt="" className="icon" />
-            </div>
-          </div>
+            </AccordionSummary>
+            <AccordionDetails>123</AccordionDetails>
+          </Accordion>
         </div>
         <div className="option">
-          <div className="title">
-            <span>Shared Files</span>
-            <img src="./arrowUp.png" alt="" />
-          </div>
+          <Accordion>
+            <AccordionSummary
+              style={{ padding: "0", color: "white" }}
+              expandIcon={<KeyboardArrowDownIcon />}
+            >
+              <div className="title">
+                <span>Privacy & Help</span>
+              </div>
+            </AccordionSummary>
+            <AccordionDetails>123</AccordionDetails>
+          </Accordion>
+        </div>
+        <div className="option">
+          <Accordion>
+            <AccordionSummary
+              style={{ padding: "0", color: "white" }}
+              expandIcon={<KeyboardArrowDownIcon />}
+            >
+              <div className="title">
+                <span>Shared photos</span>
+              </div>
+            </AccordionSummary>
+            <AccordionDetails>
+              <div className="photos">
+                <div className="photoItem">
+                  <div className="photoDetail">
+                    <img
+                      src="https://images.pexels.com/photos/7381200/pexels-photo-7381200.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
+                      alt=""
+                    />
+                    <span>photo_2024_2.png</span>
+                  </div>
+                  <img src="./download.png" alt="" className="icon" />
+                </div>
+              </div>
+            </AccordionDetails>
+          </Accordion>
         </div>
         <button onClick={handleBlock}>
           {isCurrentUserBlocked

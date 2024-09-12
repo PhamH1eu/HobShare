15774bb34e156fdd450408048c698025a03bb114
Phạm Minh Hiếu 21020319 <21020319@vnu.edu.vn>
2024-09-12 23:39:13 +0700

    firebase hosting (not working though)

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import commonjs from "vite-plugin-commonjs";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), commonjs({
+    filter(id) {
+      if (id.includes('node_modules/@pmhieu/react-comments-section')) {
+        return true;
+      }
+    },
+  })],
   define: {
     // Some libraries use the global object, even though it doesn't exist in the browser.
     // Alternatively, we could add `<script>window.global = window;</script>` to index.html.
     // https://github.com/vitejs/vite/discussions/5912
     global: {},
+  },
+  build: {
+    commonjsOptions: { transformMixedEsModules: true } // Change
+  },
+  optimizeDeps: {
+    include: ["@pmhieu/react-comments-section"],
   },
   resolve: {
     alias: {

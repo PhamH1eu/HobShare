473246fc01349daadd42352a9ea1625106a3d1f8
Phạm Minh Hiếu <hieupham110303@gmail.com>
2024-09-19 17:05:48 +0700

    change functions file

--- functions/index.js
+++ functions/index.js
@@ -1,29 +1,6 @@
-const functions = require("firebase-functions");
 const admin = require("firebase-admin");
+const onPostCreated = require("./onPostCreated");
 
 admin.initializeApp();
 
-exports.onPostCreated = functions.firestore
-  .document("posts/{postId}")
-  .onCreate(async (snap, context) => {
-    const postId = context.params.postId;
-    const data = snap.data();
-
-    // Check if tags exist and are an array
-    if (data.tags && Array.isArray(data.tags)) {
-      const promises = data.tags.map(async (tag) => {
-        const hashtagRef = admin
-          .firestore()
-          .doc(`hashtag/${tag}/posts/${postId}`);
-        return hashtagRef.set({
-          createdAt: admin.firestore.FieldValue.serverTimestamp(),
-          postId: postId,
-        });
-      });
-
-      // Wait for all the tag document creations to complete
-      await Promise.all(promises);
-    }
-
-    return null;
-  });
+exports.onPostCreated = onPostCreated.onPostCreated;
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -7,6 +7,7 @@ const HomePage = styled.div`
   display: flex;
   margin-top: 64px;
   overflow-y: auto;
+  width: 100%;
   &::-webkit-scrollbar-thumb {
     border-radius: 10px;
   }
